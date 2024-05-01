0a5fee499fd81d625cad588dda6d4059ab5303f5
farih khoirul huda <83053504+farihkhoirulhuda@users.noreply.github.com>
2024-05-01 22:17:36 +0700

    login.js

---
+++ login.js
@@ -0,0 +1,25 @@
+document.getElementById('login-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const email = document.getElementById('login-email').value;
+  const password = document.getElementById('login-password').value;
+  auth.signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log(user);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
+
+document.getElementById('login-google').addEventListener('click', () => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  auth.signInWithPopup(provider)
+    .then((result) => {
+      const user = result.user;
+      console.log(user);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
